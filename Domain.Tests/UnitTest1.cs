51ad3d9a0dc494c7ca4824779b9b77c49cfdcdca
Nuno Esteves <nuno.esteves@cognizant.com>
2026-04-09 23:36:23 +0100

    Add project files.

---
+++ Domain.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            var a = new Class1();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
